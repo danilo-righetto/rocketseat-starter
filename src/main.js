48b7cc7bedcdda2dd1f5693c70de9168e1d0ebd3
Danilo Righetto <danilonewtrue@gmail.com>
2019-08-17 22:22:08 -0300

    Render em tela

--- src/main.js
+++ src/main.js
@@ -255,6 +255,7 @@ class App {
   constructor() {
     this.repositories = [];
     this.formEl = document.getElementById('repo-form');
+    this.listEl = document.getElementById('repo-list');
     this.registerHandlers();
   }
 
@@ -270,7 +271,35 @@ class App {
       avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
       html_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4'
     });
+
+    this.render();
     console.log('addRepository', this.repositories);
+  }
+
+  render() {
+    this.listEl.innerHTML = '';
+    this.repositories.forEach(repo => {
+      let imgEl = document.createElement('img');
+      imgEl.setAttribute('src', repo.avatar_url);
+
+      let titleEl = document.createElement('strong');
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement('p');
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement('a');
+      linkEl.setAttribute('target', '_blank');
+      linkEl.appendChild(document.createTextNode('Acessar'));
+
+      let listItemEl = document.createElement('li');
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+    });
   }
 }
 
