71a8ce1688bd76a3e955b7b9bc551452d3830f14
Danilo Righetto <danilonewtrue@gmail.com>
2019-08-17 22:40:20 -0300

    Buscando na API

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { somaFunc, subFunc, div as dvision } from './funcoes';
 import somaExemplo from './soma';
 import * as funcoes from './funcoes';
 import axios from 'axios';
+import apiApp from './api';
 class List {
   constructor() {
     this.data = [];
@@ -256,6 +257,7 @@ class App {
     this.repositories = [];
     this.formEl = document.getElementById('repo-form');
     this.listEl = document.getElementById('repo-list');
+    this.inputEl = document.getElementById('repository');
     this.registerHandlers();
   }
 
@@ -263,14 +265,24 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
-  addRepository (event) {
+  async addRepository (event) {
     event.preventDefault();
+    const repoInput = this.inputEl.value;
+    if (repoInput.length == 0) return;
+
+    const response = await apiApp.get(`/repos/${repoInput}`);
+    console.log('addRepository - test', response);
+
+    const { name, description, html_url, owner: { avatar_url } } = response.data;
+
     this.repositories.push({
-      name: 'danilo-righetto',
-      description: 'Danilo Righetto - Analista de Sistemas',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
-      html_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4'
+      name,
+      description,
+      avatar_url,
+      html_url
     });
+
+    this.inputEl.value = '';
 
     this.render();
     console.log('addRepository', this.repositories);
@@ -290,6 +302,7 @@ class App {
 
       let linkEl = document.createElement('a');
       linkEl.setAttribute('target', '_blank');
+      linkEl.setAttribute('href', repo.html_url);
       linkEl.appendChild(document.createTextNode('Acessar'));
 
       let listItemEl = document.createElement('li');
