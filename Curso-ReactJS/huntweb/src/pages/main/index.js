bf743194d529a93e82bba47fc709448bbb7ba5c7
Danilo Righetto <drighetto@allin.com.br>
2019-08-20 14:40:08 -0300

    Armazenando no estado

--- Curso-ReactJS/huntweb/src/pages/main/index.js
+++ Curso-ReactJS/huntweb/src/pages/main/index.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component {
+  // Armazenando no estado
+  state = {
+    products: []
+  };
+
   // Metodo executado assim que o componente for executado em tela
   componentDidMount(){
     this.loadProducts();
@@ -9,10 +14,22 @@ export default class Main extends Component {
 
    loadProducts = async () => {
     const response = await api.get(`/products`);
+    
+    // Atualizando a variavel de estado ('state')
+    this.setState({ products: response.data.docs });
+
     console.log('loadProducts', response.data.docs);
   };
 
+  // O render pode depender do 'state' (Estado) da aplicação
   render() {
-    return <h1>Main - Hello Rocketseat</h1>;
+    return (
+      <div className="product-list">
+        <h1>Main - Contagem de produtos: {this.state.products.length}</h1>;
+        {this.state.products.map(product => (
+          <h2 key={product._id}>{product.title}</h2>
+        ))}
+      </div>
+    );
   }
 }
